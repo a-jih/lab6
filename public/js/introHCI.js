f7c0eb5db1145ded6406dc916b87379cabab675f
ajh018@ucsd.edu <andyjihwang@gmail.com>
2018-02-15 11:01:51 -0800

    completed lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addProject);
+	
+}
+
+function addProject(result) {
+	console.log(result);
+  var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+		'</small></p></a>';
+
+		$(`#project${result['id']} .details`).html(projectHTML);
 }
